89c1a844f58802a60ea613e7cf8c3410444daa22
Archishman Sengupta <71402528+ArchishmanSengupta@users.noreply.github.com>
2021-07-12 22:52:59 +0530

    refactor: firebaseConfig

--- src/index.js
+++ src/index.js
@@ -1,31 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+//import firebase from 'firebase';
+import 'firebase/app'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from "firebase/app";
+import "firebase/analytics";
+import "firebase/auth";
+import "firebase/firestore";
 
-const firebase = require('firebase');
+//var firebase = require('firebase/app');
 require('firebase/firestore');
 require('dotenv').config();
 
 //console.log(process.env);
 
-firebase.initializeApp(
-  {
-    apiKey: process.env.API_KEY, // please visit .env_Sample for API_KEY
+const firebaseConfig = {
+  apiKey: process.env.API_KEY, // please visit .env_Sample for API_KEY
     authDomain: "paperwork-ac719.firebaseapp.com",
     projectId: "paperwork-ac719",
     storageBucket: "paperwork-ac719.appspot.com",
     messagingSenderId: "194976899556",
     appId: "1:194976899556:web:417eb86a81b9dc9f3667f1",
     measurementId: "G-K9XYWPXYBG"
-  }
-);
+};
+firebase.initializeApp(firebaseConfig);
+
+// React DOM takes <APP /> and renders it
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />,
+  document.getElementById('paperwork container')
 );
 
 // If you want to start measuring performance in your app, pass a function
