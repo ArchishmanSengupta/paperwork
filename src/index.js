11cef3f2c5febc55f54136144b610a685dd60730
Archishman Sengupta <71402528+ArchishmanSengupta@users.noreply.github.com>
2021-07-12 22:26:28 +0530

    initialize: firebase API_KEY

--- src/index.js
+++ src/index.js
@@ -4,6 +4,23 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const firebase = require('firebase');
+require('firebase/firestore');
+require('dotenv').config();
+
+//console.log(process.env);
+
+firebase.initializeApp(
+  {
+    apiKey: process.env.API_KEY, // please visit .env_Sample for API_KEY
+    authDomain: "paperwork-ac719.firebaseapp.com",
+    projectId: "paperwork-ac719",
+    storageBucket: "paperwork-ac719.appspot.com",
+    messagingSenderId: "194976899556",
+    appId: "1:194976899556:web:417eb86a81b9dc9f3667f1",
+    measurementId: "G-K9XYWPXYBG"
+  }
+);
 ReactDOM.render(
   <React.StrictMode>
     <App />
