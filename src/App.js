211baf6755f29ff8335589da13ba4bde56103761
Archishman Sengupta <71402528+ArchishmanSengupta@users.noreply.github.com>
2021-07-13 01:14:43 +0530

    component-load-lifecycle_hook__

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
 
 import './App.css';
 import React from 'react';
+import "firebase/firestore";
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+//const firebase = require('firebase');
+require("firebase/firestore");
 class App extends React.Component {
+  constructor(){
+    // super() will call the constructor of its parent class. 
+    // This is required when you need to access some variables from the parent class.
+    super(); 
+    this.state={
+      selectNoteIndex:null,
+      selectedNode:null,
+      notes:null,
+    }
+  }
+
   render() {
     return(
-      <div> Testing</div>
+      <div>HMMMMM</div>
     );
+  }
+
+  //Lifecycle hook: we want our App component to get called before
+  //when the component is loaded immediately 
+
+  componentDidMount=() => {
+    firebase
+    .firestore()
+    .collection('notes')                    // Table in a db
+    .onSnapshot(serverUpdate=>{             // function will be automatically called when notes is updated
+      const notes = serverUpdate   
+      .docs                                 // prop of serverUpdate obj that is getting passed
+      .map(_doc => {
+        const data=_doc.data();             //grabs data from the docs
+        data['id']=_doc.id;             
+        return data;
+      });
+      console.log(notes);
+      this.setState({notes:notes});          // mapping notes state with const notes line 36
+    });
   }
 }
 
