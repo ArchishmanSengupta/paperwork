fa1f2a488d0a22d0f726d030f6c5c1d59c8c6185
Archishman Sengupta <71402528+ArchishmanSengupta@users.noreply.github.com>
2021-07-13 12:41:55 +0530

    add: subsec with sidebar  boiler plate

---
+++ src/sidebar/sidebar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import styles from './styles';
+import List from '@material-ui/core/List';
+import {Divider,Buttons} from '@material-ui/core';
+import SidebarItemComponent from '../sidebarItems/Sidebaritems';
+
+
+class SidebarComponent extends React.Component {
+    constructor() {
+        super();
+    }
+    render(){
+        return (<div> Sidebar</div>);
+    }
+}
+export default withStyles(styles)(SidebarComponent);
