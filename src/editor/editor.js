1e9677412c3d823e078054f88f1a03cb0b5ab00b
Archishman Sengupta <71402528+ArchishmanSengupta@users.noreply.github.com>
2021-07-13 12:34:44 +0530

    add: subsec with editor boiler plate

---
+++ src/editor/editor.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactQuill from 'react-quill';
+import debounce from '../helper'
+import BorderColoration from '@material-ui/icons/BorderColor';
+import { withStyles } from '@material-ui/core/styles';
+import styles from './styles';
+
+class EditorComponent extends React.Component {
+    constructor() {
+        super();
+    }
+    render(){
+        return (<div>Editor Test</div>);
+    }
+}
+export default withStyles(styles)(EditorComponent);     // takes agrs stles, Styles is a function in which we will be using in the styles.js file
